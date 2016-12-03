7fba0890b25be85ae9cfd9d1d9deab8a6f59c63b
Martin Pogorzelski <martinp321@gmail.com>
2016-12-03 15:13:48 -0500

    Add project files.

---
+++ Assets/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+    public Atom[] atomPrefabs;
+    float timeSinceLastSpawn;
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            Spawn();
+
+        }
+    }
+
+    void Spawn()
+    {
+        Atom prefab = atomPrefabs[Random.Range(0, atomPrefabs.Length)];
+        Atom spawn = Instantiate<Atom>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
